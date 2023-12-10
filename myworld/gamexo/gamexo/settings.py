baf0526897fa90c413a5b00f2464af48af6cd52d
Haddouche Maamar <frocodeof@gmail.com>
2023-12-10 23:46:02 +0100

    Update

--- myworld/gamexo/gamexo/settings.py
+++ myworld/gamexo/gamexo/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'gamexo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'NAME': 'incomeexpensesdb',
+        'USER': 'postgres',
+        'PASSWORD': 'frocode',
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'PORT': 5432,
     }
 }
 
