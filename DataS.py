762483b7e322fbcc4e5ae4bc99f3d46d5b300a0c
Haddouche Maamar <frocodeof@gmail.com>
2023-12-24 23:57:42 +0100

    Import Pandas

--- DataS.py
+++ DataS.py
@@ -1,14 +1,1 @@
-from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-infrared_thermography_temperature = fetch_ucirepo(id=925) 
-  
-# data (as pandas dataframes) 
-X = infrared_thermography_temperature.data.features 
-y = infrared_thermography_temperature.data.targets 
-  
-# metadata 
-print(infrared_thermography_temperature.metadata) 
-  
-# variable information 
-print(infrared_thermography_temperature.variables) 
+import pandas as pd
