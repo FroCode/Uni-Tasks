3a8b7b19d63f50a216a639b845e339b9e17a0a46
Haddouche Maamar <frocodeof@gmail.com>
2023-12-25 17:55:04 +0100

    Export CSV Fille

--- DataS.py
+++ DataS.py
@@ -34,9 +34,74 @@ print("Range of Alcohol content: {}".format(alcohol_range))
 malic_acid_mean = df['Malic acid'].mean()
 
 print("Average amount of Malic acid: {}".format(malic_acid_mean))
+color_intensity_values = df['Color intensity']
+
 plt.figure(figsize=(8, 6))
 sns.histplot(color_intensity_values, bins=20, kde=True, color='blue')  # Adjust the number of bins as needed
 plt.title('Distribution of Color intensity')
 plt.xlabel('Color intensity')
 plt.ylabel('Frequency')
 plt.show()
+
+alcohol_content = df['Alcohol']
+color_intensity = df['Color intensity']
+
+# Calculate the correlation coefficient
+correlation_coefficient = alcohol_content.corr(color_intensity)
+
+print("Correlation coefficient between Alcohol content and Color intensity: {}".format(correlation_coefficient))
+phenolic_compounds = df[['Total phenols', 'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins']]
+
+# Calculate the correlation matrix
+correlation_matrix = phenolic_compounds.corr()
+
+# Create a heatmap for visualization
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=0.5)
+plt.title('Correlation Matrix of Phenolic Compounds')
+plt.show()
+
+magnesium_values = df['Magnesium']
+
+# Create a histogram for Magnesium values
+plt.figure(figsize=(8, 6))
+plt.hist(magnesium_values, bins=20, color='green', edgecolor='black')  # Adjust the number of bins as needed
+plt.title('Distribution of Magnesium Values')
+plt.xlabel('Magnesium')
+plt.ylabel('Frequency')
+plt.show()
+
+# Assuming 'OD280/OD315 of diluted wines' is the column you're interested in
+od_values = df['OD280/OD315 of diluted wines']
+
+# Create a histogram for OD280/OD315 values
+plt.figure(figsize=(8, 6))
+plt.hist(od_values, bins=20, color='orange', edgecolor='black')  # Adjust the number of bins as needed
+plt.title('Distribution of OD280/OD315 Values')
+plt.xlabel('OD280/OD315')
+plt.ylabel('Frequency')
+plt.show()
+od_values = df['OD280/OD315 of diluted wines']
+
+# Create a histogram for OD280/OD315 values
+plt.figure(figsize=(8, 6))
+plt.hist(od_values, bins=20, color='orange', edgecolor='black')  # Adjust the number of bins as needed
+plt.title('Distribution of OD280/OD315 Values')
+plt.xlabel('OD280/OD315')
+plt.ylabel('Frequency')
+plt.show()
+
+
+# magnesium_values = df['Magnesium']
+
+# # Get the frequency values using value_counts
+# frequency_values = magnesium_values.value_counts()
+
+# # Create a DataFrame to store the frequency values
+# frequency_df = pd.DataFrame({'Magnesium': frequency_values.index, 'Frequency': frequency_values.values})
+
+# # Export the frequency values to a CSV file
+# frequency_df.to_csv('magnesium_frequency_values.csv', index=False)
+
+# Export OD280/OD315 values to a CSV file
+# od_values.to_csv('od_values_distribution_values.csv', index=False)
