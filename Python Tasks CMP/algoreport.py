86c5c8b6d0a4c01dbc5e7ab9961c190c95fd98c8
Haddouche Maamar <frocodeof@gmail.com>
2023-12-28 11:18:41 +0100

    PART 2

--- Python Tasks CMP/algoreport.py
+++ Python Tasks CMP/algoreport.py
@@ -1,21 +1,48 @@
-def pascal_triangle(n):
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
-    return triangle
+def next_generation(board):
+    n, m = len(board), len(board[0])
+    new_board = [['.' for _ in range(m)] for _ in range(n)]
 
-def print_pascal_triangle(n):
-    triangle = pascal_triangle(n)
-    for row in triangle:
-        for num in row:
-            if num % 2 == 0:
-                print("E", end=" ")
-            else:
-                print("O", end=" ")
-        print()
+    for i in range(n):
+        for j in range(m):
+            alive_neighbors = 0
+            
+            # Counting alive neighbors
+            for x in [-1, 0, 1]:
+                for y in [-1, 0, 1]:
+                    if x == y == 0:
+                        continue
+                    ni, nj = (i + x) % n, (j + y) % m
+                    if board[ni][nj] == 'X':
+                        alive_neighbors += 1
+            
+            # Applying the rules of Game of Life
+            if board[i][j] == 'X' and alive_neighbors in [2,3]:
+                new_board[i][j] = 'X'
+            elif board[i][j] == '.' and alive_neighbors == 3:
+                new_board[i][j] = 'X'
 
-print_pascal_triangle(30)
+    return new_board
+
+# Example usage:
+initial_board = [
+    ['.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', 'X', '.', '.'],
+    ['.', 'X', '.', 'X', '.', '.'],
+    ['.', '.', 'X', 'X', '.', '.'],
+    ['.', '.', '.', '.', ".", "."]
+]
+
+next_gen_board = next_generation(initial_board)
+for row in next_gen_board:
+    print(''.join(row))
+
+
+# To determine the number of live neighbors for the cell in the second row and nineteenth column in the thirty-seventh generation, 
+# we would need to know the initial configuration of the board. Without this information
+# it is impossible to provide an accurate answer.
+#Sooo
+# The Game of Life is known for its complex and unpredictable behavior, 
+# and it is difficult to predict exactly when a particular pattern will stabilize. However, 
+# some patterns are known to be stable or oscillating. 
+# For example, the “block” pattern is a stable pattern that remains unchanged from one generation to the next.
+#     The “blinker” pattern is an oscillating pattern that alternates between two states every two generations.
